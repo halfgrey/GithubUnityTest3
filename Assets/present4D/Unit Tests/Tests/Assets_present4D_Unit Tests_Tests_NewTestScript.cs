ab1e2562d34578ad6f0a2278cad8dbf0981f0eaa
DESKTOP-J3635RV\Angelo <angelo.overmeier@gmail.com>
2018-07-18 14:09:44 +0200

    Added Test Script that runs in simulated play mode

--- Assets/present4D/Unit Tests/Tests/Assets_present4D_Unit Tests_Tests_NewTestScript.cs
+++ Assets/present4D/Unit Tests/Tests/Assets_present4D_Unit Tests_Tests_NewTestScript.cs
@@ -3,10 +3,11 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
 
-public class Assets_present4D_UnitTests_Editor_Tests_NewTestScript {
+public class Assets_present4D_UnitTests_Tests_NewTestScript {
 
     [Test]
-    public void Assets_present4D_UnitTests_Editor_Tests_NewTestScriptSimplePasses() {
+    public void Assets_present4D_UnitTests_Editor_Tests_NewTestScriptSimplePasses()
+    {
         // Use the Assert class to test conditions.
         string test1 = "equal";
         string test2 = "equal";
@@ -17,7 +18,8 @@ public class Assets_present4D_UnitTests_Editor_Tests_NewTestScript {
     // A UnityTest behaves like a coroutine in PlayMode
     // and allows you to yield null to skip a frame in EditMode
     [UnityTest]
-    public IEnumerator Assets_present4D_UnitTests_Editor_Tests_NewTestScriptWithEnumeratorPasses() {
+    public IEnumerator Assets_present4D_UnitTests_Editor_Tests_NewTestScriptWithEnumeratorPasses()
+    {
         // Use the Assert class to test conditions.
         // yield to skip a frame
         yield return null;
